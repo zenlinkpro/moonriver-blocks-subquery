0eb3ee7bf1b6fe245008e4bf7d582c3e0ae3b2f8
Hayden Fan <haosong0323@gmail.com>
2022-06-15 17:10:24 +0800

    chore: init

---
+++ src/mappings/blocks.ts
@@ -0,0 +1,10 @@
+import { Block } from "../types";
+import { SubstrateBlock } from "@subql/types";
+
+export async function handleBlock(block: SubstrateBlock): Promise<void> {
+  const id = block.hash.toHex()
+  const blockEntity = new Block(id);
+  blockEntity.number = block.block.header.number.toBigInt();
+  blockEntity.timestamp = BigInt( block.timestamp.toString());
+  blockEntity.save();
+}
