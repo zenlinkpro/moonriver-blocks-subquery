0d9c3aa9b37e279ea3d18abf7edb709f07ffa128
Hayden Fan <haosong0323@gmail.com>
2022-06-15 18:48:10 +0800

    fix: timestamp

--- src/mappings/blocks.ts
+++ src/mappings/blocks.ts
@@ -2,9 +2,9 @@ import { Block } from "../types";
 import { SubstrateBlock } from "@subql/types";
 
 export async function handleBlock(block: SubstrateBlock): Promise<void> {
-  const id = block.hash.toHex()
+  const id = block.block.header.hash.toString()
   const blockEntity = new Block(id);
   blockEntity.number = block.block.header.number.toBigInt();
-  blockEntity.timestamp = BigInt( block.timestamp.toString());
+  blockEntity.timestamp = BigInt(block.timestamp.getTime())
   await blockEntity.save();
 }
